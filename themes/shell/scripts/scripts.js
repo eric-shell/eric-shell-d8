ed32edd6b1554501d7b33faded71d85d6148ea32
Eric Shell <ericjshell@gmail.com>
2016-03-15 22:01:15 -0700

    Adding initial header theming

--- themes/shell/scripts/scripts.js
+++ themes/shell/scripts/scripts.js
@@ -2,4 +2,19 @@
   $( document ).ready(function( $ ) {
 
   });
+
+  var lastScrollTop = 0;  
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+
+    if (st > lastScrollTop && st > 0){
+      $('#block-shell-main-menu').removeClass('hidden-main-menu');
+    }
+    else {
+      $('#block-shell-main-menu').addClass('hidden-main-menu');
+    }
+
+    lastScrollTop = st;
+  });
+
 })(jQuery);
