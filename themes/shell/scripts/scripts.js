7ac2f17357da672e31f14346773d061bed202672
Eric Shell <ericjshell@gmail.com>
2016-05-04 17:10:47 -0700

    Utilizing old main menu visibility functionality for the menu toggle

--- themes/shell/scripts/scripts.js
+++ themes/shell/scripts/scripts.js
@@ -1,12 +1,26 @@
 (function ($) {
 
-  $( document ).ready(function( $ ) {
+  $(document).ready(function($) {
 
     // Toggle main menu
     $('#menu-toggle').click(function() {
       $('body').toggleClass('menu-active');
     });
 
+  });
+
+  var lastScrollTop = 0;  
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+
+    if (st > lastScrollTop && st > 0){
+      $('#menu-toggle').removeClass('menu-toggle-active');
+    }
+    else {
+      $('#menu-toggle').addClass('menu-toggle-active');
+    }
+
+    lastScrollTop = st;
   });
 
 })(jQuery);
